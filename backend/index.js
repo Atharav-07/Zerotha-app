0dba6b2b4113975a5bb5b8eb340d41b81fa4d69d
unknown <kamalchougle96@gamil.com>
2026-07-06 17:02:45 +0530

    added

--- backend/index.js
+++ backend/index.js
@@ -20,7 +20,7 @@ app.use(bodyParser.json());
 app.use(cors({
     origin: [
         "http://localhost:5173",
-        "https://your-frontend.vercel.app"
+        "https://zerotha-app.vercel.app/"
     ],
     credentials: true
 }));
